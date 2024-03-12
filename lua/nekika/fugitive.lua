0bed6f618fceaab28a30c5ef72b7bd6ecdef78df
Nekika <hugo.jahnke@gmail.com>
2024-03-12 12:21:03 +0100

    Created fugitive :GcLog mapping

--- lua/nekika/fugitive.lua
+++ lua/nekika/fugitive.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>")
+vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gl", vim.cmd.GcLog);
+vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>");
