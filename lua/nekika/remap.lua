aba28d879014d9d0b037ef1a66b872c20a8d4327
Nekika <hugo.jahnke@gmail.com>
2024-03-13 22:23:57 +0100

    Created remaps to navigate between buffers

--- lua/nekika/remap.lua
+++ lua/nekika/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "[b", vim.cmd.bpreviou)
+vim.keymap.set("n", "]b", vim.cmd.bnext)
