407440c232cd656774227d85552cbb9ce9139b0d
Nekika <hugo.jahnke@gmail.com>
2024-03-15 09:49:26 +0100

    Updated lua lsp config

--- lua/nekika/lsp.lua
+++ lua/nekika/lsp.lua
@@ -5,12 +5,21 @@ require("lspconfig").gopls.setup({})
 require("lspconfig").lua_ls.setup({
   settings = {
     Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
       diagnostics = {
         globals = {
-          "require",
-          "vim",
-        }
-      }
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
     }
   }
 })
