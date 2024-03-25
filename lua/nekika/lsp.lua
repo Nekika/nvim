73df87a91eb89b5d05d4639d176eccecfe841270
Nekika <hugo.jahnke@gmail.com>
2024-03-25 15:25:05 +0100

    Set up zls config

--- lua/nekika/lsp.lua
+++ lua/nekika/lsp.lua
@@ -24,6 +24,8 @@ require("lspconfig").lua_ls.setup({
   }
 })
 
+require("lspconfig").zls.setup({})
+
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     vim.api.nvim_create_autocmd({ "BufWrite", "BufWritePre" }, {
