8f614f21d36828b3b21ebed2c7f0fa22b82640b4
Nekika <hugo.jahnke@gmail.com>
2024-03-25 15:25:25 +0100

    Updated treesitter config

--- lua/nekika/treesitter.lua
+++ lua/nekika/treesitter.lua
@@ -1,8 +1,8 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = {"css", "gleam", "go", "html", "javascript", "lua", "typescript", "vim"},
-    sync_install = true,
-    auto_install = true,
-    highlight = {
-        enable = true
-    }
+  ensure_installed = { "gleam", "go", "lua", "vim", "zig" },
+  sync_install = true,
+  auto_install = true,
+  highlight = {
+    enable = true
+  }
 })
