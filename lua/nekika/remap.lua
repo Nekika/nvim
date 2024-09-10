01e6be4495ae9a1c272cce0542919cdd225a0244
Nekika <hugo.jahnke@gmail.com>
2024-09-10 10:29:51 +0200

    Auto formatted some files

--- lua/nekika/remap.lua
+++ lua/nekika/remap.lua
@@ -1,19 +1,22 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
-vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>p', vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>h", vim.cmd.noh)
+vim.keymap.set('n', '<leader>h', vim.cmd.noh)
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
-vim.keymap.set("n", "[b", vim.cmd.bpreviou)
-vim.keymap.set("n", "]b", vim.cmd.bnext)
+vim.keymap.set('n', '[b', vim.cmd.bpreviou)
+vim.keymap.set('n', ']b', vim.cmd.bnext)
 
-vim.keymap.set("n", "[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end)
-vim.keymap.set("n", "]e", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end)
+vim.keymap.set('n', '[e', function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end)
+vim.keymap.set('n', ']e', function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end)
 
-vim.keymap.set("n", "[w", "<C-w>W")
-vim.keymap.set("n", "]w", "<C-w>w")
+vim.keymap.set('n', '[w', '<C-w>W')
+vim.keymap.set('n', ']w', '<C-w>w')
 
-vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-n>")
+vim.keymap.set({ 'n', 'v' }, '<C-y>', '"ay')
+vim.keymap.set({ 'n', 'v' }, '<C-p>', '"ap')
+
+vim.keymap.set('t', '<ESC><ESC>', '<C-\\><C-n>')
