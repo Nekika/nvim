33581a5e42c58d0df0c2745717ef26b628195bee
Nekika <hugo.jahnke@gmail.com>
2024-09-28 10:38:05 +0200

    Set keymaps to got to previous/next diagnostic

--- lua/nekika/remap.lua
+++ lua/nekika/remap.lua
@@ -12,8 +12,14 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '[b', vim.cmd.bpreviou)
 vim.keymap.set('n', ']b', vim.cmd.bnext)
 
-vim.keymap.set('n', '[e', function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end)
-vim.keymap.set('n', ']e', function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end)
+vim.keymap.set('n', '[e', function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
+  { desc = 'Go to previous error' })
+
+vim.keymap.set('n', ']e', function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end,
+  { desc = 'Go to next error' })
+
+vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, { desc = 'Go to previous diagnostic' })
+vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, { desc = 'Go to next diagnostic' })
 
 vim.keymap.set('n', '[w', '<C-w>W')
 vim.keymap.set('n', ']w', '<C-w>w')
